45d8074d48aec05a9cc18ec3e29857916bc599ca
noguimc <18989638256@163.com>
2025-07-27 11:18:18 +0800

    None

--- 27.2.cpp
+++ 27.2.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    long long a;
-    cin>>a;
-    long long b;
-    cin>>b;
-    long long c=0;
-    for(long long i=a;i<=b;i++){
-        c=c*10;
-        c=c+i;
+    long long a, b, c = 0;
+    cin >> a;
+    cin >> b;
+
+    for(long long i = a; i <= b; i++){
+        long long p = 1; 
+        long long d = i;
+        while(d != 0){
+            p *= 10; 
+            d /= 10;
+        }
+        c = c * p + i; 
     }
-    cout<<c%9;
+    cout << c;
+    return 0;
 }
