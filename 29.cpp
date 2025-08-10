7a972d7891b6fe982dc331a992ea4f34444d5a5e
noguimc <18989638256@163.com>
2025-08-10 09:08:50 +0800

    None

---
+++ 29.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[1000]={0},b;string n;
+    cin>>n>>b;
+    int len=n.length();
+    
+    for(int i=0;i<len;i++){
+        a[len-i]=n[i]-'0';
+    }
+    
+    for(int i=1;i<=len;i++){
+        a[i]=a[i]*b;
+    }
+    
+    for(int i=1;i<=len;i++){
+        a[i+1]+=a[i]/10;
+        a[i]%=10;
+    }
+    
+    while(a[len+1]){
+        len++;
+        a[len+1]+=a[len]/10;
+        a[len]%=10;
+    }
+    
+    while(a[len]==0&&len>1) len--;
+    
+    for(int i=len;i>=1;i--){
+        cout<<a[i];
+    }
+}
