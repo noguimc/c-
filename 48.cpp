5ce62510b7eefdf6a99c1a78fb8852a053a7df1b
noguimc <18989638256@163.com>
2025-11-30 10:12:40 +0800

    None

---
+++ 48.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int c[a];
+    for(int i=0;i<a;i++){
+        cin>>c[i];
+    }
+    
+
+
+    return 0;
+}
